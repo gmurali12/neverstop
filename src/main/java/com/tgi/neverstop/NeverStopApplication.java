61eda60216d32ca07dcc81bdfa6658e50ed4420f
deva0409 <devapradeepaj1995@gmail.com>
2020-10-07 16:06:34 +0530

    Swagger Dependency Added In pom.xml

--- src/main/java/com/tgi/neverstop/NeverStopApplication.java
+++ src/main/java/com/tgi/neverstop/NeverStopApplication.java
@@ -2,13 +2,47 @@ package com.tgi.neverstop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
+@EnableSwagger2
 @SpringBootApplication
 public class NeverStopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NeverStopApplication.class, args);
 	}
+	
+	@Bean
+	   public Docket trainingApi() {
+	        return new Docket(DocumentationType.SWAGGER_2)
+	                .securitySchemes(Collections.singletonList(new ApiKey("JWT", "Authorization", "header")))
+	                .securityContexts(Collections.singletonList(
+	                        SecurityContext.builder()
+	                                .securityReferences(
+	                                        Collections.singletonList(SecurityReference.builder()
+	                                                .reference("JWT")
+	                                                .scopes(new AuthorizationScope[0])
+	                                                .build()
+	                                        )
+	                                )
+	                                .build())
+	                )
+	                .select()
+	                .apis(RequestHandlerSelectors
+	                        .basePackage("com.tgi.neverstop"))
+	                .paths(PathSelectors.regex("/.*"))
+	                .build();
+	    }
 
 }
